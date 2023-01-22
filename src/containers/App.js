2fa1de443b6d134dab17b187c1395ee3e6b245d0
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2023-01-22 17:19:24 +0200

    use React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,56 +6,52 @@ import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
 
-class App extends Component {
+function App() {
 
+    // using destructuring
+    const [robots, setRobots ] = useState([]);
+    const [searchField, setSearchField ] = useState('');
+    const [count, setCount ] = useState(0);
 
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
-    }
 
     // make requests on componentDidMount
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => {this.setState({ robots: users })});
+        .then(users => {setRobots(users)});
+        console.log(count);
+    }, [count]); // only run if count changes.
+
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    }
+    
+    
+    if (!robots.length) {
+        // add loading bar here
+        return <h1>Loading...</h1>
+    } else {
+
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        });
+
+        return (
+            <div className='tc'>
+                <h1 className="f1">RoboFriends</h1>
+                <button onClick={() => setCount(count + 1)}>Click Me!</button>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
     }
 
-    render() {
 
-        // using destructuring
-        const {robots, searchField } = this.state;
-        
-        if (!robots.length) {
-            // add loading bar here
-            return <h1>Loading...</h1>
-        } else {
-
-            const filteredRobots = robots.filter(robot => {
-                return robot.name.toLowerCase().includes(searchField.toLowerCase())
-            });
-
-            return (
-                <div className='tc'>
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
-
-    }
 
 }
 
